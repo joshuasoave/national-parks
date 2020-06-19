ce19270df7d194a951e99140e5266e0d87b09868
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-06-18 21:58:24 -0400

    trying project 3 db

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express()
 const mongoose = require('mongoose')
 const db = mongoose.connection
 require('dotenv').config()
-const MONGODB_URI  = process.env.MONGODB_URI
+const PROJECT3_DB  = process.env.MONGODB_URI
 const PORT = process.env.PORT
 
 //~~~~~~~~DB Config~~~~~~~~~~//
 
-mongoose.connect(MONGODB_URI,  { useNewUrlParser: true});
+mongoose.connect(PROJECT3_DB,  { useNewUrlParser: true});
 
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
@@ -24,7 +24,7 @@ app.use(express.static('public'))
 
 //~~~~~~~~~Controllers~~~~~~~~~//
 
-//controllers 
+//controllers
 
 //~~~~~~~~~Listener~~~~~~~~~//
 
