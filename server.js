b1aa11534e55316c0947626cf547f3d8b3697cba
Roy Sanfilipo <roysanfilipo@gmail.com>
2020-06-18 22:43:07 -0400

    fixing issues

--- server.js
+++ server.js
@@ -5,16 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 const db = mongoose.connection
 require('dotenv').config()
-const PROJECT3_DB  = process.env.PROJECT3_DB
-const PORT = process.env.PORT
-
-//~~~~~~~~DB Config~~~~~~~~~~//
-
-mongoose.connect(PROJECT3_DB,  { useNewUrlParser: true});
-
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
-db.on('disconnected', () => console.log('mongo disconnected'));
+const MONGODB_URI  = process.env.MONGODB_URI
 
 //~~~~~~~~~Middleware~~~~~~~~~//
 
@@ -22,12 +13,25 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
+//~~~~~~~~DB Config~~~~~~~~~~//
+
+mongoose.connect(MONGODB_URI,  { useNewUrlParser: true});
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
 //~~~~~~~~~Controllers~~~~~~~~~//
 
 //controllers
 
+app.get('/', (req, res) => {
+  res.send('hi')
+})
+
 //~~~~~~~~~Listener~~~~~~~~~//
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log('listening to ariana grande...');
 })
